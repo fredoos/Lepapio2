247a2df572e98310b2c9d29d41129a3d199a2eca
fredoos <25873219+fredoos@users.noreply.github.com>
2025-10-13 20:02:16 +0200

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  }
 });
 
